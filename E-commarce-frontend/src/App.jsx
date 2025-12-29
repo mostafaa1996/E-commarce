0c95b4fb838d68e4d6417ee0284caa1f39bb716f
Mostafa Hamdy <mostafa.hamdy@brightskiesinc.com>
2025-12-29 22:40:54 +0200

    modify folder structure

--- E-commarce-frontend/src/App.jsx
+++ E-commarce-frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import HomeHeader from "../components/Home/Header"
-import Navbar from "../components/Top layer/Navbar"
-import TapInfo from "../components/Top layer/tap_Info"
+import Navbar from "../components/Sections/Top layer/Navbar"
+import TapInfo from "../components/Sections/Top layer/tap_Info"
 import LimitedSale from "../components/Home/LimitedSale"
 import FeaturesSection from "../components/Home/Features"
 import ReviewsSection from "../components/genericComponents/CustomersReviews"
-import Brands from "../components/genericComponents/Brands"
-import Footer from "../components/genericComponents/footer"
-import TitleOfPages from "../components/genericComponents/TitleOfPages"
+import Brands from "../components/Sections/Brands"
+import Footer from "../components/Sections/footer"
+import TitleOfPages from "../components/Sections/TitleOfPages"
 
 function App() {
   
